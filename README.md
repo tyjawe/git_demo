89fd7d7616d73e24980747c2ef1f436bfcc5ce0d
Tyler Wendell <tyler.wendell09@gmail.com>
2021-03-13 20:30:12 -0500

    Initial Push

---
+++ README.md
@@ -0,0 +1,9 @@
+# Basic Calculator
+
+This simple program takes in two numbers and performs one of four basic operations on them. 
+
+## Implementation plan:
+[ ] Add
+[ ] Subtract
+[ ] Multiplication
+[ ] Division
---
+++ basic.py
@@ -0,0 +1,24 @@
+# Basic Calculator
+
+print("Please enter your operation")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("Q Quit")
+
+while True:
+    # Take input from the user
+    choice = input("Enter choice(1/2/3/4/Q): ")
+    if choice.lower() == "q":
+        break
+    first = input("Enter first number: ")
+    second = input("Enter second number: ")
+    if choice == "1":
+        print("Not Implemented yet")
+    if choice == "2":
+        print("Not Implemented yet")
+    if choice == "3":
+        print("Not Implemented yet")
+    if choice == "4":
+        print("Not Implemented yet")
